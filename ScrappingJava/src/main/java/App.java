ac77ffc8a0c5458a70c7d2498a50bcb79117bafe
xxbadgame <52980268+xxbadgame@users.noreply.github.com>
2024-01-24 01:54:56 +0100

    Add files via upload

---
+++ ScrappingJava/src/main/java/App.java
@@ -0,0 +1,11 @@
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlTable;
+import com.gargoylesoftware.htmlunit.html.HtmlTableRow;
+import com.gargoylesoftware.htmlunit.html.HtmlTableDataCell;
+
+public class App {
+    public static void main(String[] args) {
+       System.out.println("Hello");
+    }
+}
